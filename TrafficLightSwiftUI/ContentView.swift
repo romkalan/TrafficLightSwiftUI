c5fb02e1bdee59dd129b726eb493283cb0ed5ccd
romkalan <romkalan@mail.ru>
2023-04-20 13:30:13 +0300

    add logic on the button

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,17 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var opacityForRedLight: Double = 0.3
+    @State var opacityForYellowLight: Double = 0.3
+    @State var opacityForGreenLight: Double = 0.3
+    
+    @State var buttonText = "START"
+    
     var body: some View {
         ZStack {
             Color(.black)
                 .ignoresSafeArea()
             VStack {
-                LightView(color: .red)
-                LightView(color: .yellow)
-                LightView(color: .green)
+                LightView(color: .red, opacity: opacityForRedLight)
+                LightView(color: .yellow, opacity: opacityForYellowLight)
+                LightView(color: .green, opacity: opacityForGreenLight)
+                
                 Spacer()
+                                
+                Button(action: changeColorLight) {
+                    Text(buttonText)
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 50)
+                        .background(.blue)
+                        .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.white,lineWidth: 4)
+                        )
+                }
+                .padding()
             }
             .padding(.top)
+            
+        }
+    }
+    
+    private func changeColorLight() {
+        let turnOff: Double = 0.3
+        let turnOn: Double = 1
+        
+        buttonText = "NEXT"
+    
+        if opacityForRedLight == turnOn {
+            opacityForRedLight = turnOff
+            opacityForGreenLight = turnOff
+            opacityForYellowLight = turnOn
+        } else if opacityForYellowLight == turnOn {
+            opacityForRedLight = turnOff
+            opacityForYellowLight = turnOff
+            opacityForGreenLight = turnOn
+        } else {
+            opacityForYellowLight = turnOff
+            opacityForGreenLight = turnOff
+            opacityForRedLight = turnOn
         }
     }
 }
--- TrafficLightSwiftUI/LightView.swift
+++ TrafficLightSwiftUI/LightView.swift
@@ -9,18 +9,22 @@ import SwiftUI
 
 struct LightView: View {
     let color: Color
+    let opacity: Double
     
     var body: some View {
         Circle()
             .foregroundColor(color)
             .frame(width: 100, height: 100)
             .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .opacity(opacity)
             .padding(.bottom)
     }
 }
 
+
+
 struct LightView_Previews: PreviewProvider {
     static var previews: some View {
-        LightView(color: .cyan)
+        LightView(color: .cyan, opacity: 0.3)
     }
 }
